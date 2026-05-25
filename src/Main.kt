815c56f16d36b66dfd3324cc768d26b4e75d357d
jiangrui <jiangrui@xinye.com>
2026-05-25 21:10:50 +0800

    init

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val name = "Kotlin"
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+
+    var a = 1;
+    val s1 = "a is $a"
+    a = 2
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+    println(s2)
+}
