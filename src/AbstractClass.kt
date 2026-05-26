4369693dc06d1e83ccb8e74c22b6cc8d2d181dea
jiangrui <jiangrui@xinye.com>
2026-05-26 17:29:39 +0800

    对一个对象实例调用多个方法 （with）

--- src/AbstractClass.kt
+++ src/AbstractClass.kt
@@ -4,6 +4,21 @@ abstract class MyAbstractClass {
 
 }
 
+class Student {
+    /*
+    * Kotlin 中 var 属性自动生成 getter/setter，不需要手写 setName()/setAge()，
+    * 直接用 stu.name = "小明" 赋值即可。
+    * */
+    var name = ""
+    var age = 0
+
+    fun showInfo() {
+        println("姓名：$name，年龄：$age")
+    }
+}
+
+
+
 fun main() {
     /*
     * object：匿名对象
@@ -18,4 +33,24 @@ fun main() {
         }
     }
     myObject.doSomething()
+
+    // 实例化 Student 类
+    val stu = Student()
+
+    // 普通写法（要一直写 stu.）
+    stu.name = "小明"
+    stu.age = 18
+    stu.showInfo()
+
+    println("-----------")
+
+    // with 写法（不用写 stu.）
+    with(stu) {
+        name = "小红" // 直接赋值属性
+        age = 20
+        showInfo()
+    }
+
 }
+
+
