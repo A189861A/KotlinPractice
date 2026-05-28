f3977f1e421b6d95669267666c9c21bb53139225
jiangrui <jiangrui@xinye.com>
2026-05-28 11:13:30 +0800

    枚举

---
+++ src/EnumClassSample.kt
@@ -0,0 +1,48 @@
+// 普通枚举
+enum class Color {
+    RED, GREEN, BLUE // 固定常量
+}
+
+//带参数的枚举
+//1. 枚举类定义主构造函数：接收 code、msg 两个参数
+enum class State(val code: Int, val msg: String) {
+    // 2. 每一个枚举项 = 调用构造函数创建实例，传参 code、msg 两个参数
+    SUCCESS(200, "请求成功"),
+    ERROR(500, "服务器错误"),
+    LOADING(100, "加载中"); // 最后一项加分号，后续可以写成员/方法
+
+    // 可选：自定义方法
+    fun getInfo(): String {
+        return "编码：$code，描述：$msg";
+    }
+
+    // 静态查找：根据 code 查找对应的举项
+    companion object {
+        // 1. 定义一个静态方法，接收 code 参数，返回对应的举项
+        fun fromCode(code: Int): State? {
+            return values().firstOrNull { it.code == code }
+        }
+    }
+}
+
+fun main() {
+    val color = Color.RED
+
+    // 判断
+    when (color) {
+        Color.RED -> println("红色")
+        Color.GREEN -> println("绿色")
+        Color.BLUE -> println("蓝色")
+    }
+
+    println(State.SUCCESS.code) // 200
+    println(State.ERROR.msg)  // 服务器错误
+
+    println(State.LOADING.getInfo()) // 编码：100，描述：加载中
+
+    // 静态查找：根据 code 查找对应的举项
+    val state = State.fromCode(200)
+    println(state?.getInfo()) // 编码：200，描述：请求成功
+    println(state?.msg) // 请求成功
+
+}
